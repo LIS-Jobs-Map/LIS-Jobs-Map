3225c9a0a08737ee96cbd7e11ad4ca809c728398
cmurgu <cmurgu@live.ca>
2023-04-20 11:26:16 -0400

    new js

--- deploy.js.js
+++ deploy.js.js
@@ -1,10 +1,16 @@
 const ghpages = require('gh-pages');
+const { GITHUB_TOKEN } = process.env;
+
+if (!GITHUB_TOKEN) {
+  console.error('GITHUB_TOKEN environment variable is not set');
+  process.exit(1);
+}
 
 ghpages.publish(
   'data',
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/cmurgu/mapping-jobs.git',
+    repo: `https://${GITHUB_TOKEN}@github.com/cmurgu/mapping-jobs.git`,
     user: {
       name: 'GitHub Action',
       email: 'action@github.com',
